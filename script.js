083dffd66e48083f7795fd094e348c7127b4a65d
Alex <mkumum12@mail.ru>
2020-03-04 20:17:57 +0300

    Added initial movement of game: miss shots added

---
+++ script.js
@@ -0,0 +1,54 @@
+const record = document.getElementById("record"),
+      shot = document.getElementById("shot"),
+      hit = document.getElementById("hit"),
+      dead = document.getElementById("dead"),
+      enemy = document.getElementById("enemy"),
+      again = document.getElementById("again");
+
+const play = {
+    record: 0,
+    shot: 0,
+    hit: 0,
+    dead: 0,
+    set updateData(data) {
+        this[data]++;
+        this.render();
+    },
+    render() {
+        record.textContent = this.record;
+        shot.textContent = this.shot;
+        hit.textContent = this.hit;
+        dead.textContent = this.dead;
+    }
+};
+
+const show = {
+    hit() {
+        
+    },
+    miss(element) {
+        this.changeClass(element, 'miss');
+    },
+    dead() {
+        
+    },
+    changeClass(element, value) {
+        element.className = value;
+    }
+}
+
+const fire = (event) => {
+    const target = event.target;
+    if(target.className == 'miss') {
+
+    } else {
+        show.miss(target);
+        play.updateData = 'shot';
+    }
+};
+
+const init = () => {
+    enemy.addEventListener('click', fire);
+};
+
+init();
