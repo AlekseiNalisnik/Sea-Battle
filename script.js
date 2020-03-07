aa7e32b2049dd099dc473b8ed708175244dd7c63
Alex <mkumum12@mail.ru>
2020-03-07 20:20:31 +0300

    First release, added random spawn for ships, u can play solo for ur own record, all bug fixed

--- script.js
+++ script.js
@@ -8,25 +8,88 @@ const record = document.getElementById("record"),
 
 
 const game = {
-    ships: [
-        {
-            location: ['26', '36', '46', '56'],
-            hit: ['', '', '', '']
-        },
-        {
-            location: ['11', '12', '13'],
-            hit: ['', '', '']
-        },
-        {
-            location: ['69', '79'],
-            hit: ['', '']
-        },
-        {
-            location: ['32'],
-            hit: ['']
+    ships: [],
+    shipCount: 0,
+    optionShip: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1]
+    },
+    collision: new Set(),                       // Новая коллекция
+    generateShip() {
+        // Количество кораблей
+        for(let i = 0; i < this.optionShip.count.length; i++) {
+            // Количество кораболей определённого размера
+            for(let j = 0; j < this.optionShip.count[i]; j++) {
+                const size = this.optionShip.size[i];
+                const ship = this.generetaOptionsShip(size);
+                this.ships.push(ship);
+                this.shipCount++;
+            }
         }
-    ],
-    shipCount: 4, 
+    },
+    generetaOptionsShip(shipSize) {
+        const ship = {
+            hit: [],
+            location: []
+        };
+
+        // direction - true или false
+        const direction = Math.random() < 0.5;
+        // Координаты корабля
+        let x, y;
+
+        if(direction) {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize));
+        } else {
+            x = Math.floor(Math.random() * (10 - shipSize));
+            y = Math.floor(Math.random() * 10);
+        }
+
+        // Добавление координат в объект
+        for(let i = 0; i < shipSize; i++) {
+            if(direction) {
+                ship.location.push(x + '' + (y + i));
+            } else {
+                ship.location.push((x + i) + '' + y);
+            }
+            ship.hit.push('');
+        }
+
+        // Проверка на расположение кораблей
+        if(this.checkCollision(ship.location)) {
+            return this.generetaOptionsShip(shipSize);
+        }
+
+        // Добавление координат, куда нельзя ставить корабль, Безопасная зона
+        this.addCollision(ship.location);
+        
+        return ship;
+    },
+    checkCollision(location) {
+        for(const coord of location) {
+            if(this.collision.has(coord)) {
+                return true;
+            }
+        }
+        //return false;
+    },
+    addCollision(location) {
+        // Обработка безопасной зоны идёт тремя прямоугольниками
+        for(let i = 0; i < location.length; i++) {
+            const startCoordX = location[i][0] - 1;
+            for(let j = startCoordX; j < startCoordX + 3; j++) {
+                const startCoordY = location[i][1] - 1;
+                for(let k = startCoordY; k < startCoordY + 3; k++) {
+                    if(j >= 0 && j < 10 && k >= 0 && k < 10) {
+                        const coord = j + '' + k;
+                        // Проверка на многократное добавление одинаковых координат безопасной зоны через коллекцию
+                        this.collision.add(coord);
+                    }
+                }
+            }
+        }
+    } 
 }
 
 const play = {
@@ -102,9 +165,16 @@ const fire = (event) => {
 const init = () => {
     enemy.addEventListener('click', fire);
     play.render();
+    game.generateShip();
 
     again.addEventListener('click', () => {
         location.reload();
+    });
+
+    record.addEventListener('dblclick', () => {
+        localStorage.clear();
+        play.record = 0;
+        play.render();
     });
 };
 
